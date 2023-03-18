050ede6c23508f12d5a9cad10dbe72c3af0ead7a
HalleluyahBoy <eejimone@gmail.com>
2023-03-18 10:00:39 +0100

    Deployment of Database smart contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
 const SchoolDatabase = artifacts.require("SchoolDatabase");
 const SchoolDatabaseMigration = artifacts.require("SchoolDatabaseMigration");
 
-module.exports = function (deployer) {
-  deployer.deploy(SchoolDatabase).then(function() {
-    return deployer.deploy(SchoolDatabaseMigration, SchoolDatabase.address, SchoolDatabase.address);
-  });
+module.exports = async function (deployer) {
+  await deployer.deploy(SchoolDatabase);
+  const schoolDatabase = await SchoolDatabase.deployed();
+  await deployer.deploy(SchoolDatabaseMigration, schoolDatabase.address);
 };
